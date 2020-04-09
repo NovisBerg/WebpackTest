61e242e0117a1d997f22b9e7c6666503497ddf9b
BergNovis <jacktwist.ennis@outlook.com>
2020-04-09 12:12:02 +0800

    最后提交

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = { // __dirname是node.js中的一个全局变量，它指向当前执行脚本所在的目录
     devtool: 'eval-source-map',
 
     entry: __dirname + '/app/main.js', // 已多次提及的唯一入口文件
     output: {
-        path: __dirname + '/public', // 打包后的文件存放的地方
+        path: __dirname + '/build', // 打包后的文件存放的地方
         filename: 'bundle.js' // 打包后输出文件的文件名
     },
 
@@ -13,7 +14,8 @@ module.exports = { // __dirname是node.js中的一个全局变量，它指向当
         contentBase: './public', // 本地服务器所加载的页面所在的目录
         historyApiFallback: true, // 不跳转
         inline: true, // 实时刷新
-        port: 8097 // 设置监听端口，若省略则默认为8080
+        port: 8097, // 设置监听端口，若省略则默认为8080]
+        hot: true
     },
     module: {
         rules: [
@@ -44,6 +46,10 @@ module.exports = { // __dirname是node.js中的一个全局变量，它指向当
         ]
     },
     plugins: [
-        new webpack.BannerPlugin('版权所有，翻版必究')
+        new webpack.BannerPlugin('版权所有，翻版必究'),
+        new HtmlWebpackPlugin({
+            template: __dirname + "/app/index.tmpl.html" // new一个这个插件的实例，并传入相关的参数
+        }),
+        new webpack.HotModuleReplacementPlugin() // 热加载插件
     ]
 }
